16d2ff885186065da05b3a9c0e270e6fe14f1430
程序员小勇 <44111382+FZSWJLY@users.noreply.github.com>
2022-02-05 15:43:44 +0800

    Delete Day 02 Basic Introduction - Data Package Expansion/burp-2021-2 directory

--- Day 02 Basic Introduction - Data Package Expansion/burp-2021-2/burpsuite.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-start "burpsuite" /B "javaw.exe" -Xmx8G -XX:-UseParallelGC -noverify -javaagent:BurpSuiteLoader.jar -Dfile.encoding=utf-8 -jar "%~dp0burpsuite_pro_v2021.2.jar" %*
-exit 0 
